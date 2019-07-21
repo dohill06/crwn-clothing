7573aebb3a414291f80959ad9506d7e73dc32d35
Douglas Hill <dohill06@gmail.com>
2019-07-21 09:21:32 -0700

    Set up shop component with data

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import SHOP_DATA from './shop.data';
+
+class ShopPage extends React.Component {
+  state = {
+    collections: SHOP_DATA
+  };
+
+  render() {
+    return <div>SHOP PAGE</div>;
+  }
+}
+
+export default ShopPage;
